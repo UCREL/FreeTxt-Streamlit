3823cb9ada990870a3acb27c144916c6d54ba04e
Nouran-Khallaf <63323128+Nouran-Khallaf@users.noreply.github.com>
2023-05-26 00:31:55 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -291,7 +291,7 @@ unsafe_allow_html=True)
         st.image("img/FreeTxt_logo.png", width=300) 
     # Analysis page content and layout
     st.write("---")
-    bt1,bt2,bt3,bt4,bt5,bt6 = st.columns([2,2,1,1,1,1])
+    bt1,bt2,bt3,bt4,bt5,bt6 = st.columns([2,2,2,2,2,2])
     with bt1:
             if st.button('Home'):
                 st.experimental_set_query_params(page=None)

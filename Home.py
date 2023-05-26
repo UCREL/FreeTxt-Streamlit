e11de5e96026025d7682ce073cd15ea90c750c08
Nouran-Khallaf <63323128+Nouran-Khallaf@users.noreply.github.com>
2023-05-26 14:41:01 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -577,7 +577,7 @@ class txtanalysis:
         return image_path
     
     def show_kwic(self, fname):
-        context = plot_kwic(self.reviews, fname)
+        context = plot_kwic(self.reviews, fname,tab6)
         return context
     def concordance(self, fname):
         with tab8:

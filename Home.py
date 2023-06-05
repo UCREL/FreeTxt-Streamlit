6851ba39b02f5a307f0916428fd6a69949faf72b
Nouran-Khallaf <63323128+Nouran-Khallaf@users.noreply.github.com>
2023-06-05 18:43:09 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -1783,10 +1783,6 @@ any sector in Wales and beyond to use it.
     </a>
 """, unsafe_allow_html=True)
 
-	
-        #if st.button("Start Analysis", key="analysis_button", help="Redirects to the Analysis page"):
-
-          #  st.experimental_set_query_params(page="analysis")
             
     with button_col2:
        
@@ -1797,7 +1793,7 @@ any sector in Wales and beyond to use it.
     <a href="https://nouran-khallaf-free-txt-home-gdoog2.streamlit.app/?page=demo" 
        target = "_self"
        style = "
-          font-size: 30px; 
+          font-size: 40px; 
           font-family: 'Tangerine', cursive; 
           color: #4a4a4a; 
           background-color: #ddd; 
@@ -1812,8 +1808,6 @@ any sector in Wales and beyond to use it.
     </a>
 """, unsafe_allow_html=True)
 
-        if st.button("Watch a Demo", key="demo_button", help="Redirects to the Demo page"):
-            st.experimental_set_query_params(page="demo")
             
     st.write("---")
    

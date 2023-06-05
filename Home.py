727701582e3c973e95cff1edee7560cb4b4a0cc9
Nouran-Khallaf <63323128+Nouran-Khallaf@users.noreply.github.com>
2023-06-05 23:22:46 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -1811,16 +1811,7 @@ def main():
     </h1>""", 
     unsafe_allow_html=True)
     
-    selected4 = option_menu(None, ["Home", "Analysis",  "Demo"], 
-    		icons=['house', 'sliders2',  'gear'], 
-    		menu_icon="cast", default_index=0, orientation="horizontal",
-   		 styles={
-    		    "container": {"padding": "0!important", "background-color": "#fafafa"},
-   		     "icon": {"color": "orange", "font-size": "25px"}, 
-  		      "nav-link": {"font-size": "25px", "text-align": "left", "margin":"0px", "--hover-color": "#eee"},
-     		   "nav-link-selected": {"background-color": "green"},
-   		 }
-		)
+
     #24px;border: 2px solid grey;
     with open('style.css') as f:
         st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html =True)

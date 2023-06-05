b8863b882268789f150824374069c168009a34cb
Nouran-Khallaf <63323128+Nouran-Khallaf@users.noreply.github.com>
2023-06-05 23:36:01 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -2039,6 +2039,19 @@ unsafe_allow_html=True
 
 def app():
     state = get_session_state(selected3=None)
+    
+    if state.selected3 is None:
+        state.selected3 = option_menu(None, ["Home", "Analysis",  "Demo"], 
+            icons=['house', 'sliders2',  'gear'], 
+            menu_icon="cast", default_index=0, orientation="horizontal",
+            styles={
+                "container": {"padding": "0!important", "background-color": "#fafafa"},
+                "icon": {"color": "orange", "font-size": "25px"}, 
+                "nav-link": {"font-size": "25px", "text-align": "left", "margin":"0px", "--hover-color": "#eee"},
+                "nav-link-selected": {"background-color": "green"},
+            }
+        )
+
     query_params = st.experimental_get_query_params()
     page = query_params.get("page", [None])[0]
 
@@ -2048,19 +2061,25 @@ def app():
     elif page == "analysis":
         st.experimental_set_query_params(page="analysis")
         analysis_page()
-   
-    elif state.selected3 == 'Demo':
+    elif page == 'about':
+        st.experimental_set_query_params(page="about")
+        about_page()
+    elif page is None:  # If there are no query parameters, use the menu.
+        if state.selected3 == 'Demo':
             st.experimental_set_query_params(page="demo")
             demo_page()
-    elif state.selected3 == 'Analysis':
+        elif state.selected3 == 'Analysis':
             st.experimental_set_query_params(page="analysis")
             analysis_page()
-    elif state.selected3 == 'Home':
+        elif state.selected3 == 'Home':
             st.experimental_set_query_params(page="home")
             main()
-    else:
+        else:
             st.warning('Invalid option selected, returning to Home')
             main()
+    else:
+        main()
+
     
 
 

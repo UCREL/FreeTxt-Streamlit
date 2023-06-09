f629b690d7233051e929e1e64258cb406cb9789d
Nouran-Khallaf <63323128+Nouran-Khallaf@users.noreply.github.com>
2023-06-09 15:16:17 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -2134,7 +2134,7 @@ a.menu-link:hover {{
   
 }}
 </style>
-<div style="overflow: hidden; padding: 10px; background-color: #3393FF;">
+<div style="overflow: hidden; padding: 10px; background-color: #33B2FF;">
 <a class="menu-link" href="https://nouran-khallaf-free-txt-home-gdoog2.streamlit.app/?page=home" target="_self" style="float: left; color: #4a4a4a; text-align: center; padding: 14px 16px; text-decoration: none; font-size: 18px; margin: 0px; transition: 0.3s;">
     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" style="height: 25px;">
         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />

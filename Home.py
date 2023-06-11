adc5b07bf1c29ff795b4cd3c5641bba46aef9ce5
Nouran-Khallaf <63323128+Nouran-Khallaf@users.noreply.github.com>
2023-06-11 01:49:17 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -1807,7 +1807,7 @@ def analysis_page():
           
                 _, df = data[filenames[i]]
                 
-                df,selected_columns = select_columns(df, key=i).astype(str)
+                df = select_columns(df, key=i).astype(str)
                 st.write(selected_columns)
                 check_language = st.checkbox('Check file language')
                 if check_language:

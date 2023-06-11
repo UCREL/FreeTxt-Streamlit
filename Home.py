6cf0a9bae7e009e1c319516bf561c7906f596e93
Nouran-Khallaf <63323128+Nouran-Khallaf@users.noreply.github.com>
2023-06-11 01:30:13 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -1797,6 +1797,9 @@ def analysis_page():
                 _, df = data[filenames[i]]
                 handle_language_detection(df)
                 df = select_columns(df, key=i).astype(str)
+		if check_language:
+                    for key in data.keys():
+                        handle_language_detection(data[key])
                 if df.empty:
                     st.info('''**NoColumnSelected 🤨**: Please select one or more columns to analyse.''', icon="ℹ️")
                 else:

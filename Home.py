4f6db957a97a36288d7205507218250bccb35810
Nouran-Khallaf <63323128+Nouran-Khallaf@users.noreply.github.com>
2023-07-02 15:58:45 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -2178,7 +2178,7 @@ def textbox_analysis_page():
                     with tab3:
 
                        st.write('This tool, adapted from the Welsh Summarization project, produces a basic extractive summary of the review text from the selected columns.')
-                       summarized_text =run_summarizer(input_text,i)
+                       summarized_text =run_summarizer(input_text[0],i)
 	       ##show review
                     tab4.header('View all Data')
                     tab4.dataframe(df ,use_container_width=True)

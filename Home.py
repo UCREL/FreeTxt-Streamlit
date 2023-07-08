db0845a3f099aab76a29dd9c021cf731556aa0a1
Nouran-Khallaf <63323128+Nouran-Khallaf@users.noreply.github.com>
2023-07-08 19:06:12 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -2254,13 +2254,14 @@ def textbox_analysis_page():
                      
                     tab1,tab3,tab4,tab5,tab6,tab7,tab8= st.tabs(["📈 Meaning analysis",'📝 Summarisation',"📈 Data View", "☁️ Keyword Cloud",'💬 Keyword in Context & Collocation', "🌳 Word Tree",'📥 Download pdf'])
                     with tab1:
-                        num_classes = st.radio('How do you want to categorize the sentiments?', ('3 Class Sentiments (Positive, Neutral, Negative)', '5 Class Sentiments (Very Positive, Positive, Neutral, Negative, Very Negative)'))
-                        num_classes = 3 if num_classes.startswith("3") else 5
-
                         analysis_type = st.selectbox(
                                 'How would you like to analyze the text?',
                                          ('Analyze whole text', 'Analyze sentence by sentence')
                                                               )
+                        num_classes = st.radio('How do you want to categorize the sentiments?', ('3 Class Sentiments (Positive, Neutral, Negative)', '5 Class Sentiments (Very Positive, Positive, Neutral, Negative, Very Negative)'))
+                        num_classes = 3 if num_classes.startswith("3") else 5
+
+
 
                         st.write(df)
 

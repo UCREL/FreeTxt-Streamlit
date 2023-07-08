35d5b170537a039d65dcd24bb06bfc5b02853a2f
Nouran-Khallaf <63323128+Nouran-Khallaf@users.noreply.github.com>
2023-07-09 00:56:02 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -806,7 +806,7 @@ class txtanalysis:
             st.write('The graph below represents the searched word in the middle and the right and the left context for the word, the bigger the font size the more frequent the word is')
             st.write('The word frequency is represented by the weight in the tool tip')
             # Create a download button for the HTML file
-            
+            st.write(self)
             html.create_html(self, fname,search_word)
             HtmlFile = open("GFG-1.html", 'r')
             source_code = HtmlFile.read()

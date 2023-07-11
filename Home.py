dc9d20d2ab365a76f9f59e5fe9cf1eed1ed2d451
Nouran-Khallaf <63323128+Nouran-Khallaf@users.noreply.github.com>
2023-07-11 16:44:51 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -43,7 +43,7 @@ import scattertext as tt
 import spacy
 from pprint import pprint
 from st_aggrid import GridOptionsBuilder, AgGrid, GridUpdateMode, DataReturnMode
-from streamlit_aggrid import AgGrid
+
 import shutil
 from dateutil import parser
 import streamlit.components.v1 as components

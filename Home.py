30f81266fd1039b9c4c56cf0264c9671a30a83a0
Nouran-Khallaf <63323128+Nouran-Khallaf@users.noreply.github.com>
2023-07-12 16:59:50 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -2607,7 +2607,7 @@ def textbox_analysis_page():
         # Allow the user to select the measure to use
 	#measure = tab2.selectbox("Select a measure:", options=["Frequency","KENESS", "Log-Likelihood"])    
                all_words = []
-               cloud_type = tab5.selectbox('Choose Cloud category:', ['All words', 'Semantic Tags', 'Bigrams', 'Trigrams', '4-grams', 'Nouns', 'Proper nouns', 'Verbs', 'Adjectives', 'Adverbs', 'Numbers'], key=f"{key}_cloud_select")
+               cloud_type = tab5.selectbox('Choose Cloud category:', ['All words', 'Semantic Tags', 'Bigrams', 'Trigrams', '4-grams', 'Nouns', 'Proper nouns', 'Verbs', 'Adjectives', 'Adverbs', 'Numbers'])
                if cloud_type == 'All words':
                     all_words = list(set(nltk.tokenize.word_tokenize(input_data)))
                     df = calculate_measures(df,'KENESS')

ed0a86589a40efff5f4f94686e9e9bef99666334
Nouran-Khallaf <63323128+Nouran-Khallaf@users.noreply.github.com>
2023-07-21 00:36:01 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -2657,13 +2657,13 @@ def textbox_analysis_page():
 
                                     net_sentiment = sentiment_counts['Positive'] - sentiment_counts['Negative']
                             
-                                    st.header(f"Net sentiment: {net_sentiment}")
-                                    if net_sentiment > 0:
-                                         st.write(f'The net sentiment score of {net_sentiment} indicates that there are {net_sentiment} more positive sentiments than negative sentiments in the given text. This suggests that the overall sentiment of the text is positive.')
-                                    elif net_sentiment < 0:
-                                         st.write(f'The net sentiment score of {net_sentiment} indicates that there are {abs(net_sentiment)} more negative sentiments than positive sentiments in the given text. This suggests that the overall sentiment of the text is negative.')
-                                    else:
-                                        st.write('The net sentiment score is zero, which indicates an equal number of positive and negative sentiments. This suggests that the overall sentiment of the text is neutral.')
+                                    #st.header(f"Net sentiment: {net_sentiment}")
+                                    #if net_sentiment > 0:
+                                    #     st.write(f'The net sentiment score of {net_sentiment} indicates that there are {net_sentiment} more positive sentiments than negative sentiments in the given text. This suggests that the overall sentiment of the text is positive.')
+                                   # elif net_sentiment < 0:
+                                    #     st.write(f'The net sentiment score of {net_sentiment} indicates that there are {abs(net_sentiment)} more negative sentiments than positive sentiments in the given text. This suggests that the overall sentiment of the text is negative.')
+                                   # else:
+                                     #   st.write('The net sentiment score is zero, which indicates an equal number of positive and negative sentiments. This suggests that the overall sentiment of the text is neutral.')
 
 
                                       

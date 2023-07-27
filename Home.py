b9af7044bc49880d0494ef8b5df4509b29bbe2dc
Nouran-Khallaf <63323128+Nouran-Khallaf@users.noreply.github.com>
2023-07-27 14:53:56 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -1150,7 +1150,7 @@ def Pymsas_tags(text):
         merged_df = merged_df.drop(['Equivalent Tag'], axis=1)
         tags_to_remove = ['Unmatched', 'Grammatical bin', 'Pronouns', 'Period']
         merged_df = merged_df[~merged_df['USAS Tags'].str.contains('|'.join(tags_to_remove))]
-        
+        st.write(merged_df)
     elif lang_detected == 'en':
         nlp = spacy.load('en_core_web_sm-3.2.0')	
         english_tagger_pipeline = spacy.load('en_dual_none_contextual')

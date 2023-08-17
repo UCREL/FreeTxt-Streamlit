c4c57897719cfdf398fcfe8313ef765f80ec0570
Nouran-Khallaf <63323128+Nouran-Khallaf@users.noreply.github.com>
2023-08-17 21:45:11 +0100

    Update Home.py

--- Home.py
+++ Home.py
@@ -1692,10 +1692,10 @@ def plot_kwic(data, key,tab):
     try:
         with tab:
             topwords = [f"{w} ({c})" for w, c in getTopNWords(input_data, removeStops=True)]
-            st.write('The number with the word represents how many time this word appeared in the data')
-            keyword = st.selectbox('Select keyword:', topwords).split('(',1)[0].strip()
+            ##st.write('The number with the word represents how many time this word appeared in the data')
+            keyword = st.selectbox('Select keyword: [note: the number following the listed words indicated how many times it appears in the data]', topwords).split('(',1)[0].strip()
             
-            window_size = st.slider('Select window size:', 1, 10, 5)
+            window_size = st.slider('Select the number of words to the right and left of your keyword:', 1, 10, 5)
             maxInsts = st.slider('Maximum number of instances:', 5, 50, 15, 5,  key="slider1_key")
         
             kwic_instances = get_kwic(input_data, keyword, window_size, maxInsts, True)
